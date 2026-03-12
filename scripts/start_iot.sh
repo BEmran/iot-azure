e21b1e31ce6915417e03e10346c7a93c022d6e44
bara emran <bara.emran@gmail.com>
2026-03-12 22:10:41 +0300

    update folder

--- scripts/start_iot.sh
+++ scripts/start_iot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /home/iot/ws/iot-azure
+cd /home/iot/iot-azure
 
 # Activate virtual environment
-source /home/iot/ws/iot-azure/venv/bin/activate
+source /home/iot/iot-azure/venv/bin/activate
 
 # Run your Python app
 python3 azure-iot-thread-simple.py
